8a2f9fba2dc1acf3f02bde2e749e6bebcea19d8b
rittels <44720431+rittels@users.noreply.github.com>
2021-03-07 03:01:03 +0200

    compare with models version 0.0.5

--- config.js
+++ config.js
@@ -13,7 +13,8 @@ module.exports = {
     ,dependencies: [
 
         "https://d3js.org/d3.v5.min.js",
-        "https://unpkg.com/skewt-plus/dist/bundle.js"
+        "https://unpkg.com/skewt-plus/dist/bundle.js",
+        
 
         //'https://unpkg.com/windyplugin-module-infobox@0.0.8/dist/infobox.js'
         //'https://unpkg.com/windyplugin-module-pickertools@0.1.0/dist/pickerTools.js',
